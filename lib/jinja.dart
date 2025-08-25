8988c7495959b72f30e08b15370ecdb2d20dcb5e
Avo <avomandjian@gmail.com>
2025-08-25 19:47:33 +0400

    added export debug_controller

--- lib/jinja.dart
+++ lib/jinja.dart
@@ -1,5 +1,6 @@
 library;
 
+export 'package:jinja/src/debug/debug_controller.dart';
 export 'package:jinja/src/environment.dart';
 export 'package:jinja/src/exceptions.dart';
 export 'package:jinja/src/loaders.dart';
